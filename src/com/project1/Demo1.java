30fa90f956751cb4e21bc46edfb4773bfb31cd54
Rimsky <Rimsky@192.168.1.145>
2023-04-06 18:26:14 -0700

    Initial Commit

---
+++ src/com/project1/Demo1.java
@@ -0,0 +1,13 @@
+package com.project1;
+
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Test....");
+		
+		System.out.println("Test....");
+		
+	}
+
+}
